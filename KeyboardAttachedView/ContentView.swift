57d70ab1d8ec0f53e8b9ea9d5bf6ac6ff86582af
Gavin Nelson <gavin@nelson.co>
2024-08-21 12:31:57 -0700

    Initial Commit

---
+++ KeyboardAttachedView/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  KeyboardAttachedView
+//
+//  Created by Gavin Nelson on 8/21/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ KeyboardAttachedView/KeyboardAttachedViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  KeyboardAttachedViewApp.swift
+//  KeyboardAttachedView
+//
+//  Created by Gavin Nelson on 8/21/24.
+//
+
+import SwiftUI
+
+@main
+struct KeyboardAttachedViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
